320982a6d24ef0cb5bef84ff2706106a4920c5e9
Reid <temphee@gmail.com>
2023-12-20 01:08:58 +0900

    fix: throw `JournalSerializationException`

--- shared/src/commonMain/kotlin/com/reidsync/vibepulse/model/JournalSerialization.kt
+++ shared/src/commonMain/kotlin/com/reidsync/vibepulse/model/JournalSerialization.kt
@@ -1,5 +1,6 @@
 package com.reidsync.vibepulse.model
 
+import kotlinx.serialization.SerializationException
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.serializer
@@ -18,6 +19,8 @@ fun Notebook.serialize(): String {
 fun deserializeToNotebook(data: String) : Result<Notebook> {
 	return runCatching {
 		Json.decodeFromString<Notebook>(data)
+	}.onFailure {
+		throw JournalSerializationException(it.message)
 	}
 }
 
@@ -28,17 +31,20 @@ fun Journal.serialize(): String {
 fun deserializeToJournal(data: String) : Result<Journal> {
 	return runCatching {
 		Json.decodeFromString<Journal>(data)
+	}.onFailure {
+		throw JournalSerializationException(it.message)
 	}
 }
 
 fun deserializeJournalOrNull(data: String) = deserializeToJournal(data).getOrNull()
+fun deserializeNotebookOrNull(data: String) = deserializeToNotebook(data).getOrNull()
 
 @Throws(JournalSerializationException::class)
 fun deserializeJournalOrThrow(data: String) = deserializeToJournal(data).getOrThrow()
 
-fun deserializeNotebookOrNull(data: String) = deserializeToNotebook(data).getOrNull()
-
+//@Throws(Exception::class)
 @Throws(JournalSerializationException::class)
 fun deserializeNotebookOrThrow(data: String) = deserializeToNotebook(data).getOrThrow()
+
 
 class JournalSerializationException(message: String? = null) : Exception(message)
