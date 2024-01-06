ee47838bf8ad3dd6fa278c15ca33d56113b995ad
Reid <temphee@gmail.com>
2024-01-06 17:59:28 +0900

    feat: add location filed to Journal

--- shared/src/commonMain/kotlin/com/reidsync/vibepulse/notebook/journal/JournalLocation.kt
+++ shared/src/commonMain/kotlin/com/reidsync/vibepulse/notebook/journal/JournalLocation.kt
@@ -1,12 +1,15 @@
 package com.reidsync.vibepulse.notebook.journal
 
+import kotlinx.serialization.Serializable
+
 /**
  * Created by Reid on 2024/01/04.
  * Copyright (c) 2024 Reid Byun. All rights reserved.
  */
 
 
-data class UserLocation(
+@Serializable
+data class JournalLocation(
 	val latitude: Double = 0.0,
 	val longitude: Double = 0.0
 )
